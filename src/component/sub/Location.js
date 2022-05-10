ff501a5cf511b0037dcefe8d41b07341d7fac5bc
cheesebell <rinkky@naver.com>
2022-05-10 15:02:04 +0900

    카카오맵 여러위치 보여주기

--- src/component/sub/Location.js
+++ src/component/sub/Location.js
@@ -8,6 +8,8 @@ function Location() {
     const [ map, setMap ] = useState(null);
     // traffic btn toggle 선언
     const [ traffic, setTraffic ] = useState(false);
+    // 마커 이미지 추가
+    const path = process.env.PUBLIC_URL;
     
     useEffect(()=> {
         frame.current.classList.add('on');
@@ -20,7 +22,32 @@ function Location() {
 
         //kakao api로 부터 인스턴스 복사 (지도가 출력될 프레임, 옵션)
         const mapInfo = new kakao.maps.Map(container.current, options);
-        setMap(mapInfo)
+        setMap(mapInfo);
+
+        // 마커 포지션
+        const markerPosition = new kakao.maps.LatLng(
+            37.4146828,
+            126.6878551
+        );
+
+        // 마커이미지 정보 추가
+        const imgSrc = `${path}/img/marker1.png`;
+        const imgSize = new kakao.maps.Size(232, 99);
+        const imgOption = { offset: new kakao.maps.Point(-80, 150)};
+
+        const markerImg = new kakao.maps.MarkerImage (
+            imgSrc,
+            imgSize,
+            imgOption
+        );
+
+        // 마커 생성
+        const marker = new kakao.maps.Marker({
+            position: markerPosition,
+            image: markerImg
+
+        });
+        marker.setMap(mapInfo);
     },[]);
 
     const handleTraffic = () => {
@@ -30,12 +57,19 @@ function Location() {
             : map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
             
         }
+    };
+
+    const setCenter = (lat, lng) => {
+        const moveLatLon = new kakao.maps.LatLng(lat, lng);
+        map.setCenter(moveLatLon);
     }
+
 
     // 보여지는게 아니니 useEffect 선언
     useEffect(()=> {
         handleTraffic();
     },[traffic])
+
 
     return (
         <section className='location' ref={frame}>
@@ -44,7 +78,13 @@ function Location() {
                 <div id='map' ref={container}></div>
 
                 <div>Traffic</div>
-                <button onClick={()=> setTraffic(!traffic)}>ON/OFF</button>
+                {/* <button onClick={() => setTraffic(!traffic)}>
+                    {traffic ? 'OFF' : 'ON'}
+                </button> */}
+                <ul className='branch'>
+                    <li onClick={()=> setCenter(37.4146828, 126.6878551)}>1짱집</li>
+                    <li onClick={()=> setCenter(37.487626, 126.753045)}>어땨</li>
+                </ul>
             </div>
         </section>
     )
