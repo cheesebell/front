8a44462668e3dc296336089e6ad8608a0eba8d47
cheesebell <rinkky@naver.com>
2022-05-12 18:08:10 +0900

    todo-list 최종

--- src/component/main/Content.js
+++ src/component/main/Content.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 
 function Content() {
+
+  const getLocalDate = () => {
+
+    const dummyPosts = [
+        { title: 'Hello1', content: '뭐시당가.'},
+        { title: 'Hello2', content: 'Here comes description in detail.'},
+        { title: 'Hello3', content: 'Here comes description in detail.'},
+        { title: 'Hello4', content: 'Here comes description in detail.'},
+    ]
+
+    const data = localStorage.getItem('posts');
+    if (data) return JSON.parse(data);
+    else return dummyPosts;
+  }
+  const [posts] = useState(getLocalDate);
+
+  useEffect(()=> {
+    // 저장된 state값을 로컬 저장소에 저장
+      localStorage.setItem('posts', JSON.stringify(posts));
+  },[posts])
+
   return (
-    <div>Content</div>
+    <main>
+      {posts.map((post,idx) => {
+        if(idx < 3) {
+          return (
+            <article key={idx}>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+            </article>
+          )
+        }
+      }
+      )}
+    </main>
   )
 }
 
--- src/component/sub/Community.js
+++ src/component/sub/Community.js
@@ -4,23 +4,97 @@ import Layout from '../common/Layout';
 function Community() {
     const input = useRef(null);
     const textarea = useRef(null);
-    const dummyPosts = [
-        { title: 'Hello2', content: 'Here comes description in detail.'},
-        { title: 'Hello2', content: 'Here comes description in detail.'},
-    ]
-    const [ posts, setPosts ] = useState(dummyPosts);
+    const editInput = useRef(null); // 수정처리 선언
+    const editTextarea = useRef(null);
+    // 4. 메인 컴포넌트에서 로컬저장소에 저장된 데이터를 다시 state에 옮겨담음
+    let data = localStorage.getItem('posts');
+    data = JSON.parse(data);
 
+    const [ posts, setPosts ] = useState(data);
+    const [ allowed, setAllowed ] = useState(true); // 중복수정 막을 state
+
+
+    // 초기화 핸들
     const resetPosts = () => {
         input.current.value='';
         textarea.current.value='';
     };
 
+    // 생성
     const createPosts = () => {
+        const inputVal = input.current.value.trim();
+        const textareaVal = textarea.current.value.trim();
+        resetPosts();
+
+        if( !inputVal || !textareaVal ) {
+            alert ('제목과 본문을 모두 입력하세요');
+            return;
+        }
         setPosts([
-            {title: input.current.value, content: textarea.current.value},
+            {title: inputVal, content: textareaVal},
             ...posts
         ])
     }
+
+    // 삭제
+    // 삭제할 순번과 반복도 순번이 같으면 해당 값을 반환
+    const deletePosts = (idx) => {
+        setPosts (posts.filter((_, index) => index !== idx));
+    };
+
+    // 수정
+    const enableUpdate = (index) => {
+        // 수정모드일때 다른 수정버튼 비활성화
+        setAllowed(false);
+
+        setPosts (
+            posts.map((post, idx) => {
+                if (idx === index) post.enableUpdate = true;
+                return post;
+            })
+        )
+    };
+
+    // 취소
+    const disableUpdate = (index) => {
+        // 수정버튼 활성화
+        setAllowed(true);
+
+        setPosts (
+            posts.map((post, idx) => {
+                if (idx === index) post.enableUpdate = false;
+                return post;
+            })
+        );
+    };
+
+    // 수정출력
+    const updatePost = (index) => {
+
+        // 내용 없으면 경고창
+        if (!editInput.current.value.trim() || !editTextarea.current.value.trim()) {
+            alert('수정할 제목과 본문을 입력하세요.');
+            return;
+        };
+
+        setAllowed(true);
+
+        setPosts (
+            posts.map((post, idx) => {
+                if (idx === index ) {
+                    post.title = editInput.current.value;
+                    post.content = editTextarea.current.value;
+                    post.enableUpdate = false;
+                }
+                return post;
+            })
+        )
+    }
+
+    // 5. 해당 컴포넌트에서 CRUD로 데이터 변경이 일어날때마다 로컬저장소에 저장
+    useEffect(()=> {
+        localStorage.setItem('posts', JSON.stringify(posts));
+    },[posts])
 
     return (
         <Layout name={'Community'}>
@@ -41,11 +115,37 @@ function Community() {
             </div>
             
             <div className='showbox'>
-                {posts.map((posts,idx) => {
+                {posts.map((post,idx) => {
                     return (
                         <article key={idx}>
-                            <h2>{posts.title}</h2>
-                            <p>{posts.content}</p>
+                            {post.enableUpdate ? (
+                                <>
+                                <input 
+                                    type='text' 
+                                    defaultValue={post.title} 
+                                    ref={editInput}
+                                /><br/>
+                                <textarea 
+                                    defaultValue={post.content}
+                                    ref={editTextarea}
+                                /><br/>
+                                <button onClick={() => disableUpdate(idx)}>취소</button>
+                                <button onClick={() => updatePost(idx)}>저장</button>
+                                </>
+                            ):(
+                                <>
+                                <h2>{post.title}</h2>
+                                <p>{post.content}</p>
+                                <button 
+                                    onClick={() => {
+                                        // allowd 값이 true일떄만 수정 가능
+                                        if(allowed) enableUpdate(idx)
+                                    }}>
+                                    수정
+                                </button>
+                                <button onClick={() => deletePosts(idx)}>삭제</button>
+                                </>
+                            )}
                         </article>
                     )
                 })}
