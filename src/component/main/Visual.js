6142faa3ff9e519c888f7fdc703cd4dc2309c733
cheesebell <rinkky@naver.com>
2022-05-19 18:20:02 +0900

    swiper 추가

--- src/component/main/Visual.js
+++ src/component/main/Visual.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css';
 
 function Visual() {
   return (
     <figure className='myScroll'>
-      Visual
+      <Swiper
+        loop={true}
+        slidesPerView={3}
+        spaceBetween={50}
+        grabCursor={true}
+        centeredSlides={true}
+        pagination={{ clickable: true }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className='swiper'>
+          <SwiperSlide>1</SwiperSlide>
+          <SwiperSlide>2</SwiperSlide>
+          <SwiperSlide>3</SwiperSlide>
+          <SwiperSlide>4</SwiperSlide>
+          <SwiperSlide>5</SwiperSlide>
+      </Swiper>
     </figure>
   )
 }
